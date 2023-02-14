5dea446a95979b91fdf0af978e0f4f4abcb10432
TerezaZdislava <bastova.ter@gmail.com>
2023-02-14 14:53:05 -0800

    init

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByRole('heading')).toHaveTextContent(/Pupíci/);
+  expect(screen.getByRole('textbox')).toBeEnabled();
+  expect(screen.getByRole('textbox')).toHaveAccessibleName('Hledat');
+});
+
+test('search and display dog image results', () => {
+  render(<App />);
+  const input = screen.getByRole('textbox');
+  userEvent.type(input, 'rad');
+  expect(input).toHaveValue('rad');
+  expect(screen.getByRole('img', { name: 'Rado' })).toBeInTheDocument();
+  expect(screen.getByRole('img', { name: 'Radon' })).toBeInTheDocument();
+  expect(screen.getByRole('img', { name: 'Rada' })).toBeInTheDocument();
+  expect(screen.getByRole('img', { name: 'Radi' })).toBeInTheDocument();
 });
---
+++ src/Card.js
@@ -0,0 +1,64 @@
+import dogImage from './assets/siby.jpg';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import MaleIcon from '@mui/icons-material/Male';
+import FemaleIcon from '@mui/icons-material/Female';
+
+function Card({ dog }) {
+  function dogAge() {
+    const age = Math.floor(
+      (new Date() - new Date(dog.birthdate).getTime()) / 3.15576e10,
+    );
+    let descr;
+    switch (age) {
+      case 1:
+        descr = 'rok';
+        break;
+      case 2:
+        descr = 'roky';
+        break;
+      case 3:
+        descr = 'roky';
+        break;
+      case 4:
+        descr = 'roky';
+        break;
+      default:
+        descr = 'let';
+    }
+    return `${age} ${descr}`;
+  }
+
+  const favIcon =
+    dog.favourite === 1 ? (
+      <FavoriteIcon sx={{ color: 'indianred' }} />
+    ) : (
+      <FavoriteBorderIcon />
+    );
+  const genderIcon =
+    dog.gender === 'Male' ? (
+      <MaleIcon sx={{ color: 'dodgerblue' }} />
+    ) : (
+      <FemaleIcon sx={{ color: 'hotpink' }} />
+    );
+
+  return (
+    <div className="card-container">
+      <img src={dogImage} alt={dog.name} />
+      <div className="age">{dogAge()}</div>
+      <div className="info">
+        <span className="name">{dog.name}</span>
+        <span className="favIcon">{favIcon}</span>
+      </div>
+      <div className="info">
+        <span>{dog.address.city}</span>
+        <div className="info">
+          <span>{genderIcon}</span>
+          <span>{dog.gender}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
